426b44b5812b0a617526f59831e611c66a848cac
skamsie <skamsie@yahoo.com>
2015-01-10 21:58:37 +0100

    Initial commit

---
+++ scripts-others.js
@@ -0,0 +1,2 @@
+document.getElementsByTagName('body')[0].style.backgroundImage=sessionStorage.backgroundImage || '#ffffff';
+document.getElementsByTagName('body')[0].style.color=sessionStorage.color;
